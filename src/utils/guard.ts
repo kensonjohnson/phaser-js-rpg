1bf9a75d1faa5896b051ed4fa783ec97471c0abd
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-04-10 14:54:14 -0500

    Add helper function for exhausting options in if and switch statements

---
+++ src/utils/guard.ts
@@ -0,0 +1,7 @@
+export function exhaustiveGuard(_value: never): never {
+  throw new Error(
+    `Error: Reached forbidden guard function with unexpected value: ${JSON.stringify(
+      _value
+    )}`
+  );
+}
