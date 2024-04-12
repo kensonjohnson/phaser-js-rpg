78c5ae1c6f8235b41fba1cc1e4d71dfaa57de6dc
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-04-12 09:50:12 -0500

    Seperate cap image creation, add method to set percentage gage

--- src/battle/ui/health-bar.ts
+++ src/battle/ui/health-bar.ts
@@ -6,33 +6,54 @@ export class HealthBar {
   #healthBarContainer: GameObjects.Container;
   #fullWidth = 360;
   #scaleY = 0.7;
+  #leftCap: GameObjects.Image;
+  #middle: GameObjects.Image;
+  #rightCap: GameObjects.Image;
 
   constructor(scene: Scene, x: number, y: number) {
     this.#scene = scene;
 
     this.#healthBarContainer = this.#scene.add.container(x, y, []);
-    this.#createHealthBarImages(x, y);
+    this.#leftCap = this.#createLeftCap(x, y);
+    this.#middle = this.#createMiddle(this.#leftCap.x + this.#leftCap.width, y);
+    this.#rightCap = this.#createRightCap(
+      this.#middle.x + this.#middle.displayWidth,
+      y
+    );
+    this.#healthBarContainer.add([this.#leftCap, this.#middle, this.#rightCap]);
+
+    this.#setMeterPercentage(1);
   }
 
   get container() {
     return this.#healthBarContainer;
   }
 
-  #createHealthBarImages(x: number, y: number) {
-    const leftCap = this.#scene.add
+  #createLeftCap(x: number, y: number) {
+    return this.#scene.add
       .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP)
       .setOrigin(0, 0.5)
       .setScale(1, this.#scaleY);
-    const middle = this.#scene.add
-      .image(leftCap.x + leftCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
-      .setOrigin(0, 0.5)
-      .setScale(1, this.#scaleY);
-    middle.displayWidth = this.#fullWidth;
-    const rightCap = this.#scene.add
-      .image(middle.x + middle.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
-      .setOrigin(0, 0.5)
-      .setScale(1, this.#scaleY);
+  }
 
-    this.#healthBarContainer.add([leftCap, middle, rightCap]);
+  #createMiddle(x: number, y: number) {
+    return this.#scene.add
+      .image(x, y, HEALTH_BAR_ASSET_KEYS.MIDDLE)
+      .setOrigin(0, 0.5)
+      .setScale(1, this.#scaleY);
+  }
+
+  #createRightCap(x: number, y: number) {
+    return this.#scene.add
+      .image(x, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP)
+      .setOrigin(0, 0.5)
+      .setScale(1, this.#scaleY);
+  }
+
+  #setMeterPercentage(percentage = 1) {
+    const width = this.#fullWidth * percentage;
+
+    this.#middle.displayWidth = width;
+    this.#rightCap.x = this.#middle.x + this.#middle.displayWidth;
   }
 }
